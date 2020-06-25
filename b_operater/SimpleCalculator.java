c409768ee972745ecc4818e58802f1c371f18649
bxxtxx <qjarhfp4026@gmail.com>
2020-06-25 17:35:40 +0900

    homework

---
+++ b_operater/SimpleCalculator.java
@@ -0,0 +1,32 @@
+package b_operater;
+
+import java.util.Scanner;
+
+public class SimpleCalculator {
+
+	public static void main(String[] args) {
+
+		int num1, num2;
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("SIMPLE CALCULATOR");
+		
+		System.out.print("첫번째 숫자: ");
+		num1 = Integer.parseInt(scan.nextLine());
+		System.out.print("두번째 숫자: ");
+		num2 = Integer.parseInt(scan.nextLine());
+		
+		System.out.print("원하시는 연산 기호를 입력하세요: ");
+		String str = scan.nextLine();
+		
+		System.out.print(num1 + str + num2 + "=");
+		System.out.println( str.equals("+") ? num1 + num2 :
+			(str.equals("-") ? num1 - num2 : 
+			(str.equals("*") ? num1 * num2 :
+			(str.equals("/") ? Math.round(((double)num1 / num2)*1000)/1000.0 :
+			(str.equals("%") ? num1 % num2 : "기호 입력이 잘못되었습니다.")))));
+		
+		
+	}
+
+}
