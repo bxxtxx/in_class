5d9b031148ba7f5872eb3d6f0f9b878575dd64dc
bxxtxx <qjarhfp4026@gmail.com>
2020-06-29 22:32:34 +0900

    in home - baseball game

--- c_statement/Baseball.java
+++ c_statement/Baseball.java
@@ -9,90 +9,80 @@ public class Baseball {
 		/*
 		 * 135
 		 * 
-		 * 
+		 * baseball game ~
 		 * 
 		 */
 		
 		
-		
 		Scanner scan = new Scanner(System.in);
 		
-		int ball_1 = 0, ball_2 = 0, ball_3;
+		int b1 = 0, b2 = 0, b3 = 0;  //입력값
+		int x = 0, y = 0, z = 0;     //랜덤수
 		
-		int x = 0, y = 0, z = 0;
+		int scr = 0;                 //시도 회수
 		
+		boolean run = true;          //while문 제어
 		
-		boolean run = true;
 		
 		
 		x = (int)(Math.random()*8) + 1 ;
-		y =	(int)(Math.random()*8) + 1 ;
-		z = (int)(Math.random()*8) + 1 ;
 		
 		while(true){
-			x = (int)(Math.random()*8) + 1 ;
+			
 			y =	(int)(Math.random()*8) + 1 ;
 			z = (int)(Math.random()*8) + 1 ;
 			
-			if(x != y && x != z && y != z){
-				break;
-			}
+			if(x != y && x != z && y != z) break;
+			
 		}
 		
-		
+		System.out.println("############# BASEBALL GAME #############");
+		System.out.println("정답:"+ x + ""+ y +""+ z+"" + "\n");
 		
 		while(run){
 			
-			int count_s = 0, count_b =0, count_o = 3;
+			int cnt_s = 0, cnt_b =0, cnt_o = 3;
+			
+			scr ++;
 			
 			System.out.print("첫번째 숫자를 입력하세요 :");
-			ball_1 = Integer.parseInt(scan.nextLine());
+			b1 = Integer.parseInt(scan.nextLine());
 			
 			System.out.print("두번째 숫자를 입력하세요 :");
-			ball_2 = Integer.parseInt(scan.nextLine());
+			b2 = Integer.parseInt(scan.nextLine());
 			
 			System.out.print("세번째 숫자를 입력하세요 :");
-			ball_3 = Integer.parseInt(scan.nextLine());
+			b3 = Integer.parseInt(scan.nextLine());
 			
 			
 			
-			if(ball_1 == x){
-				count_s ++;
-			}
-			if(ball_2 == y){
-				count_s ++;
-			}
-			if(ball_3 == z){
-				count_s ++;
-			}
+			if(b1 == x) cnt_s ++;
+			
+			if(b2 == y) cnt_s ++;
+			
+			if(b3 == z) cnt_s ++;
+			
 
 			
 			
-			if(ball_1 == y || ball_1 == z){
-				count_b ++;
-			}
-			if(ball_2 == x || ball_2 == z){
-				count_b ++;
-			}
-			if(ball_3 == x || ball_3 == y){
-				count_b ++;
-			}
+			if(b1 == y || b1 == z) cnt_b ++;
+			
+			if(b2 == x || b2 == z) cnt_b ++;
+			
+			if(b3 == x || b3 == y) cnt_b ++;
 			
 			
 			
-			System.out.println(ball_1 + "" + ball_2+ ""+ball_3 + " : " + count_s +"S " + count_b + "B " + (count_o-(count_s+count_b))+"O\n");
 			
-			if(count_s == 3){
-				count_b =0; 
-				count_o =0;
-				System.out.println("정답입니다!!!");
+			System.out.println(b1 + "" + b2+ ""+b3 + " :     " + cnt_s +"S " + cnt_b + "B " + (cnt_o-(cnt_s+cnt_b))+"O\n");
+			
+			if(cnt_s == 3){
+				System.out.println("축하합니다! 정답입니다!!!         총 시도 회수: " + scr );
 				run = false;
 			}
 			
 			
 		}
-		
-		
 		
 		
 	}
